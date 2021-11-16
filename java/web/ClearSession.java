98b2c6d6dd415466c02ecaf0f3f1d07210a3ebe6
MKCY <lk955555@163.com>
2021-11-16 14:40:19 +0800

    add loginout and make login.jsp perfect

--- java/web/ClearSession.java
+++ java/web/ClearSession.java
@@ -9,11 +9,15 @@ import java.io.IOException;
 public class ClearSession extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("UTF-8");
+        request.getSession().invalidate();
+        response.setHeader("refresh", "0.5;url=games_wiki.jsp");
+        response.getWriter().write("<h1>退出登陆成功，马上返回</h1>");
     }
 }
